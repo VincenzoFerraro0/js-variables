6e85d8688ea283a96dda8c2d7f35c6036b4bb06a
VincenzoFerraro0 <ferrarov0@gmail.com>
2025-01-08 16:21:58 +0100

    Folder2: end 2-task

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -1,14 +1,19 @@
 // 1. Dichiarazione di variabili
 // Crea una variabile chiamata name e assegnale il tuo nome.
 // Stampa il valore di nome nella console.
-let name = "Vincenzo"
+let Name = "Vincenzo"
 
-console.log (name)
+console.log (Name)
 
 // 2. Tipi primitivi
 // Crea tre variabili chiamate number, text e bool e assegna rispettivamente
 // un numero, una stringa e un valore booleano.
 // Stampa tutti i valori nella console.
+let number = 4;
+let text = "ciao";
+let bool = true;
+
+console.log (number, text, bool) 
 
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente 
